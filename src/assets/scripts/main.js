7f0718dcd79a6a9999eee84aa76704d95fa8a7b7
jordisanchezntq <jordi.sanchez@gfk.com>
2023-03-10 12:59:12 +0100

    setting font awesome y importar partes importantes en main.scss

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -3,14 +3,7 @@
  * see commented examples below
  */
 
-// import 'some-node-module';
-// import SomeModule from 'some-node-module';
-
 /**
  * Write any other JavaScript below
  */
 
-+( function() {
-  const university = "UOC";
-  console.log(`Hello, ${university}!`);
-} )();
